1a26697284327f76160ce884632ef02cb49e66b9
Ramin Rabani <rrabani2@gmail.com>
2019-03-29 13:34:12 -0700

    Create range function

--- src/recursion.js
+++ src/recursion.js
@@ -82,7 +82,25 @@ var sumBelow = function(n) {
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
-
+  if (x===y) {
+  	return [];
+  }
+  if (x<y && x-y===-1) {
+  	return [];
+  }
+  if (x>y && x-y===1) {
+  	return [];
+  }
+  if (x<y) {
+    var array= range(x+1,y);
+    array.unshift(x+1);
+    return array;
+  }
+  if (x>y) {
+    var array= range(x-1,y);
+    array.unshift(x-1);
+    return array;
+  }
 };
 
 // 7. Compute the exponent of a number.
